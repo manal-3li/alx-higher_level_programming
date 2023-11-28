c83595db94d8d6b1c661e6d1d12908265c18b157
manal-3li <manal.alimoftah@gmail.com>
2023-11-28 14:12:27 -0800

    A

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+
 def two_digit(num):
     return str(num).zfill(2)
 
+
 for num in range(0, 100):
-    print("[{:s}], ".format(two_digit(num)), end="")
+    print("{:s}, ".format(two_digit(num)), end="")
