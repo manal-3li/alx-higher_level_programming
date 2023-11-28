c4ecc4d73cd22437764bd612869c34e0c7cb3997
manal-3li <manal.alimoftah@gmail.com>
2023-11-28 13:23:20 -0800

    a

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for char in range(ord('a'), ord('z') + 1):
+    if char == ord('q'):
+        continue
+    elif char == ord('e'):
+        continue
+    print("{:c}".format(char), end="")
