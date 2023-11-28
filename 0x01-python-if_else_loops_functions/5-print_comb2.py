22b13f8871a1da3514e093f7445cf3d29f831a3f
manal-3li <manal.alimoftah@gmail.com>
2023-11-28 14:21:19 -0800

    a

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -6,4 +6,4 @@ def two_digit(num):
 
 
 for num in range(0, 100):
-    print("{:s}, ".format(two_digit(num)), end="\n" if i == 99 else ", ")
+    print("{:s}, ".format(two_digit(num)), end="\n" if num == 99 else ", ")
