1a57b8281e901dfe10f7b476781aae2c11db9dc3
manal-3li <manal.alimoftah@gmail.com>
2023-11-28 13:46:23 -0800

    a

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-
 def hex_convert(num):
     result = hex(num)
-    print(f"{num} = {result}")
+    print("[{:<3}] = {}".format(num, result))
 
 for num in range(0, 99):
     hex_convert(num)
