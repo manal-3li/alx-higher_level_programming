5b50a4174a5068c133d091465e79d486137ed983
manal-3li <manal.alimoftah@gmail.com>
2023-11-28 16:20:23 -0800

    a

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for num1 in range(10):
     for num2 in range(10):
-        if num1<num2:
-            print("{}{}".format(num1,num2), end="\n" if num1==8 and num2==9 else", ")
+        if num1 < num2:
+            print("{}{}".format(num1, num2),
+                    end="\n" if num1 == 8 and num2 == 9 else", ")
